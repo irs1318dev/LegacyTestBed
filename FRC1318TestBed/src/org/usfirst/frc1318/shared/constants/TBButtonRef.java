c682b890c416364839fbd02740e1a2fc8d5f214f
Crawford <Crawford@Crawford-PC>
2012-12-04 19:35:16 -0800

    initAL game     pad controller

---
+++ FRC1318TestBed/src/org/usfirst/frc1318/shared/constants/TBButtonRef.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc1318.shared.constants;
+
+import org.usfirst.frc1318.reference.GamePad;
+import org.usfirst.frc1318.reference.GamePadReader;
+import org.usfirst.frc1318.reference.PS2Controller.PS2ButtonMap;
+
+public interface TBButtonRef 
+{
+	int TOGGLE = 1;
+	int PRESS_AND_HOLD = 0;
+	
+	int NUMBER_OF_BUTTONS = 3;
+	
+	int MODE_TOGGLE = 1;
+	int KICK_BUTTON = 2;
+	int COMPRESSOR_TOGGLE = 3;
+}
