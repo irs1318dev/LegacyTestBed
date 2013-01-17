513b564244b6000b94b4808025219c792e0d9aed
issaquahroboticsociety@gmail.com <issaquahroboticsociety@gmail.com>
2013-01-16 20:44:09 -0800

    Added 2013 FRC SDK

--- FRC1318TestBed/src/org/usfirst/frc1318/components/TBHelloWorldRunner.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/components/TBHelloWorldRunner.java
@@ -3,11 +3,12 @@ package org.usfirst.frc1318.components;
 public class TBHelloWorldRunner  extends RobotComponentBase{
 	
 	public void robotInit(){
+		System.out.println("Hello World from Robot Init");
 		
 	}
 	
 	public void teleopPeriodic(){
-		System.out.println("Hello World!");
+		System.out.println("Hello World from TeleopPeriodic");
 	}
 
 }
