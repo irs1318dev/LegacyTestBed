1f5e68360513fe88eae742316e767132538a3692
issaquahroboticsociety@gmail.com <issaquahroboticsociety@gmail.com>
2013-01-19 16:29:20 -0800

    Display LCD sample is working Added source files for wpilibj and networktables

--- FRC1318TestBed/src/org/usfirst/frc1318/components/TBHelloWorldRunner.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/components/TBHelloWorldRunner.java
@@ -1,14 +1,20 @@
 package org.usfirst.frc1318.components;
 
+import edu.wpi.first.wpilibj.DriverStationLCD;
+import edu.wpi.first.wpilibj.DriverStationLCD.Line;
+
 public class TBHelloWorldRunner  extends RobotComponentBase{
 	
 	public void robotInit(){
-		System.out.println("Hello World from Robot Init");
-		
+		System.out.println("Hello World from Robot Init 2");
+		DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser1, 1, "Hello World from Robot Init 2");
+		DriverStationLCD.getInstance().updateLCD();
 	}
 	
 	public void teleopPeriodic(){
-		System.out.println("Hello World from TeleopPeriodic");
+		System.out.println("Hello World from TeleopPeriodic 2");
+		DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser1, 1, "Hello World from Teleop Periodic 2");
+		DriverStationLCD.getInstance().updateLCD();
 	}
 
 }
