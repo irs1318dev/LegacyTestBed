b71c7f4adb7da6f89c05a7abd2611c61428c203f
Caroline <Caroline@Caroline>
2013-01-21 17:14:09 -0800

    edited MMLimitSwitchData and MMLimitSwitchReader

--- FRC1318TestBed/src/org/usfirst/frc1318/shared/constants/minimike/MMPortRef.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/shared/constants/minimike/MMPortRef.java
@@ -4,5 +4,7 @@ public interface MMPortRef {
 
 	//TODO these are placeholder values
 	static final int TURRET = 0;
+	static final int RIGHT_LIMIT_SWITCH = 1;
+	static final int LEFT_LIMIT_SWITCH = 2;
 	
 }
--- FRC1318TestBed/src/org/usfirst/frc1318/shared/minimike/MMLimitSwitchData.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/shared/minimike/MMLimitSwitchData.java
@@ -2,20 +2,18 @@ package org.usfirst.frc1318.shared.minimike;
 
 public class MMLimitSwitchData {
 	
-	static final int HIT_LEFT = 0;
-	static final int HIT_RIGHT = 1;
-	static final int NOT_HIT = 2;
-	static final int ERROR = 3;//both sensors hit
+	public static final int HIT_LEFT = 0;
+	public static final int HIT_RIGHT = 1;
+	public static final int NOT_HIT = 2;
+	public static final int ERROR = 3;//both sensors hit
 	
 	static int state = NOT_HIT;
 	
-	public static int getData()
-	{
+	public static int getData(){
 		return state;
 	}
 	
-	public static void setData(int newState)
-	{
+	public static void setData(int newState){
 		state = newState;
 	}
 }
