68a1fef3258cd73ac6367293a484198d4d8e4a35
Graham <Weason345@gmail.com>
2013-01-21 17:31:25 -0800

    added MMSEnsors class

--- FRC1318TestBed/src/org/usfirst/frc1318/components/minimike/MMTurretRunner.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/components/minimike/MMTurretRunner.java
@@ -1,13 +1,10 @@
 package org.usfirst.frc1318.components.minimike;
 
 import org.usfirst.frc1318.components.RobotComponentBase;
-import org.usfirst.frc1318.shared.TBSolenoidData;
 import org.usfirst.frc1318.shared.constants.minimike.MMPortRef;
 import org.usfirst.frc1318.shared.minimike.MMTurretData;
 
 import edu.wpi.first.wpilibj.CANJaguar;
-import edu.wpi.first.wpilibj.Jaguar;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
 public class MMTurretRunner extends RobotComponentBase {
