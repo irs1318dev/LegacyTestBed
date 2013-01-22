fb1645689d8838da46a9c3fdc7fdb309fa0acfe2
Caroline <Caroline@Caroline>
2013-01-21 19:11:33 -0800

    wrote MMCalculator

--- FRC1318TestBed/src/org/usfirst/frc1318/shared/minimike/MMTurretData.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/shared/minimike/MMTurretData.java
@@ -2,6 +2,8 @@ package org.usfirst.frc1318.shared.minimike;
 
 public class MMTurretData {
 	static double turnSpeed = 0;
+	public static final int TURN_RIGHT = 1;
+	public static final int TURN_LEFT = -1;
 	
 	public static void setTurnSpeed(double speed)
 	{
