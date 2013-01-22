9ea0ec747bf56548383bd49af6c6822e0e92ed6b
Graham <Weason345@gmail.com>
2013-01-21 17:04:35 -0800

    Added turret functionality

--- FRC1318TestBed/src/org/usfirst/frc1318/components/minimike/MMTurretRunner.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/components/minimike/MMTurretRunner.java
@@ -2,18 +2,36 @@ package org.usfirst.frc1318.components.minimike;
 
 import org.usfirst.frc1318.components.RobotComponentBase;
 import org.usfirst.frc1318.shared.TBSolenoidData;
+import org.usfirst.frc1318.shared.constants.minimike.MMPortRef;
+import org.usfirst.frc1318.shared.minimike.MMTurretData;
 
+import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
 public class MMTurretRunner extends RobotComponentBase {
-	private Jaguar turret;
+	private CANJaguar turret;
 	
 	public void robotInit() {
-		//TODO
+		
+		//this should make a new jaguar in percent mode
+		try {
+			turret = new CANJaguar(MMPortRef.TURRET, 
+					CANJaguar.ControlMode.kPercentVbus);
+		} catch (CANTimeoutException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void teleopPeriodic() {
-		//TODO
+		
+		//This should set the speed of the motor controlled by the jaguar
+		try {
+			turret.setX(MMTurretData.getTurnSpeed());
+		} catch (CANTimeoutException e) {
+			e.printStackTrace();
+		}
 	}
 }
