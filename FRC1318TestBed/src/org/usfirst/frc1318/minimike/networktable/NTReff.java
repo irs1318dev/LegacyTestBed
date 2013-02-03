bf153ad3e6167fd5d34db051bc448cd737eea16c
Neko <Neko81795@yahoo.com>
2013-02-02 17:32:19 -0800

    /

--- FRC1318TestBed/src/org/usfirst/frc1318/minimike/networktable/NTReff.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/minimike/networktable/NTReff.java
@@ -2,7 +2,6 @@ package org.usfirst.frc1318.minimike.networktable;
 
 public class NTReff 
 {
-	
 	//GamePad======================================================================
 	public static final String Gamepad_AtonButton = "gp.ab";
 	public static final String Gamepad_LeftButton = "gp.lb";
