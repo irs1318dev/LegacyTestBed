cb5b1fa4971d120928c41d90b9f7130f23949229
Corbin_Modica <corbmod@gmail.com>
2013-02-04 05:29:46 -0800

    Created basic outline for shooter.

---
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/calculators/ShooterCalculator.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc1318.FRC2013.calculators;
+
+public class ShooterCalculator {
+
+}
---
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/runners/ShooterMotorRunner.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc1318.FRC2013.runners;
+
+import org.usfirst.frc1318.components.RobotComponentBase;
+
+import edu.wpi.first.wpilibj.Talon;
+
+public class ShooterMotorRunner extends RobotComponentBase{
+	private Talon motor;
+	
+	public void RobotInit() {
+		
+		//motor = new Talon();
+	}
+	
+	public void teleopPeriodic() {
+		
+		//motor.set(.getShooterTurretData)
+	}
+
+	
+
+}
