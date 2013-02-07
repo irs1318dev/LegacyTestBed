427a88fffc42d113bebaaeec2dbd2bc1a481dd4f
Graham <Weason345@gmail.com>
2013-02-06 19:59:44 -0800

    Pseudocode

--- FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/calculators/ShooterCalculator.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/calculators/ShooterCalculator.java
@@ -1,11 +1,13 @@
 package org.usfirst.frc1318.FRC2013.calculators;
 
+import org.usfirst.frc1318.FRC2013.shared.ShooterData;
 import org.usfirst.frc1318.components.RobotComponentBase;
-
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Talon;
 
 
 public class ShooterCalculator extends RobotComponentBase {
 	
+	//if button pressed
+	//ShooterData.setShooterAngle(ShooterData.constants.SHOOTER_UP);
+	//if button pressed
+	//ShooterData.setShooterAngle(ShooterData.constants.SHOOTER_DOWN);
 }
--- FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/shared/ShooterData.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/shared/ShooterData.java
@@ -1,13 +1,18 @@
 package org.usfirst.frc1318.FRC2013.shared;
 
 public class ShooterData {
-	public static int encoderValue;
-	public static boolean shooterAngle;
+	public static int encoderValue = 0;
+	public static boolean shooterAngle = false;
 	
 	public interface constants{
 		
 		public static final boolean SOLENOID_UP = true;
 		public static final boolean SOLENOID_DOWN = false;
 	
+	}
+	
+	public void setShooterAngle(boolean upOrDown)
+	{
+		shooterAngle = upOrDown;
 	}
 }
