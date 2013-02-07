3eed716568299ab59e95d4c5713deefd2ad09a8c
Graham <Weason345@gmail.com>
2013-02-06 19:48:05 -0800

    Began implementation of shooterData

--- FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/shared/ShooterData.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/shared/ShooterData.java
@@ -2,4 +2,12 @@ package org.usfirst.frc1318.FRC2013.shared;
 
 public class ShooterData {
 	public static int encoderValue;
+	public static boolean shooterAngle;
+	
+	public interface constants{
+		
+		public static final boolean SOLENOID_UP = true;
+		public static final boolean SOLENOID_DOWN = false;
+	
+	}
 }
