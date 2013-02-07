7aa33881e1c5128549f997e5ad3afc5eef878ccf
Graham <Weason345@gmail.com>
2013-02-06 22:32:12 -0800

    Added new Pseudocode

--- FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/calculators/DriveTrainCalculator.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/calculators/DriveTrainCalculator.java
@@ -1,9 +1,22 @@
 package org.usfirst.frc1318.FRC2013.calculators;
 
 import org.usfirst.frc1318.components.RobotComponentBase;
+import org.usfirst.frc1318.generic.controllers.PID;
+
+import edu.wpi.first.wpilibj.Encoder;
 
 public class DriveTrainCalculator extends RobotComponentBase{
 	
+	Encoder rightEnc, leftEnc;//?
+	PID rightPID, leftPID;
 	
+	//find and set PID constants
+			//rightPid = new PID(kp, ki, kd, kf);
+			//rightPid.setKFade(kFade);
+		//same thing for left, probably same constants
+	
+	//input data to PIDs
+	
+	//set variable in data to be read by 
 
 }
--- FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/components/DriveTrainRunner.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/components/DriveTrainRunner.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc1318.FRC2013.components;
 
-public class DriveTrainRunner {
+import org.usfirst.frc1318.components.RobotComponentBase;
 
+import edu.wpi.first.wpilibj.Talon;
+
+public class DriveTrainRunner extends RobotComponentBase {
+	//two motors
+	private Talon right, left;
+	
+	//read speed from a data class and update
 }
