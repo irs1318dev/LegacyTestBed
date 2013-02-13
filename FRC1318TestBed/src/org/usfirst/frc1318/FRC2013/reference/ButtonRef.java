ce72ab0b256fe456f62f0ba938a2364b2211c9a2
IRS1318Dev <IRS1318Dev@ihs-2303-48744.ishs.isd.issaquah.wednet.edu>
2013-02-12 18:55:45 -0800

    lifter -> lift

--- FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/reference/ButtonRef.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/reference/ButtonRef.java
@@ -4,8 +4,8 @@ import org.usfirst.frc1318.generic.reference.PS2Controller.PS2ButtonMap;
 
 public interface ButtonRef {
 	
-	int LIFTER_UP= PS2ButtonMap.CIRCLE;
-	int LIFTER_DOWN = PS2ButtonMap.TRIANGLE;
+	int LIFT_UP= PS2ButtonMap.CIRCLE;
+	int LIFT_DOWN = PS2ButtonMap.TRIANGLE;
 	
 	int SHOOTER_UP = PS2ButtonMap.SQUARE;
 	int SHOOTER_DOWN = PS2ButtonMap.X;
