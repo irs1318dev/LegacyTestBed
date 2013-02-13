2c14ff9637dc1468bd4daaf72c746542f91264a5
IRS1318Dev <IRS1318Dev@ihs-2303-48744.ishs.isd.issaquah.wednet.edu>
2013-02-12 17:43:48 -0800

    LifterCalculatorTest

---
+++ FRC1318TestBedTest/src/org/usfirst/frc1318/FRC2013/calculatorsTest/LifterCalculatorTest.java
@@ -0,0 +1,88 @@
+package org.usfirst.frc1318.FRC2013.calculatorsTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+import org.usfirst.frc1318.FRC2013.calculators.LifterCalculator;
+import org.usfirst.frc1318.FRC2013.shared.ReferenceData;
+
+public class LifterCalculatorTest {
+	LifterCalculator lifterCalculator;
+	
+	@Before
+	public void setUpLifterCalculatorTest() {
+		lifterCalculator = new LifterCalculator();
+	}
+	
+	@Test
+	public void testShouldLiftAlreadyFalseLiftUpFalseLiftDownFalse() {
+		ReferenceData.getInstance().getLifterData().setShouldLift(false);
+		ReferenceData.getInstance().getJoystickData().setLiftUp(false);
+		ReferenceData.getInstance().getJoystickData().setLiftDown(false);
+		lifterCalculator.teleopPeriodic();
+		assertFalse(ReferenceData.getInstance().getLifterData().getShouldLift());
+	}
+	
+	@Test
+	public void testShouldLiftAlreadyFalseLiftUpFalseLiftDownTrue() {
+		ReferenceData.getInstance().getLifterData().setShouldLift(false);
+		ReferenceData.getInstance().getJoystickData().setLiftUp(false);
+		ReferenceData.getInstance().getJoystickData().setLiftDown(true);
+		lifterCalculator.teleopPeriodic();
+		assertFalse(ReferenceData.getInstance().getLifterData().getShouldLift());
+	}
+	
+	@Test
+	public void testShouldLiftAlreadyFalseLiftUpTrueLiftDownFalse() {
+		ReferenceData.getInstance().getLifterData().setShouldLift(false);
+		ReferenceData.getInstance().getJoystickData().setLiftUp(true);
+		ReferenceData.getInstance().getJoystickData().setLiftDown(false);
+		lifterCalculator.teleopPeriodic();
+		assertTrue(ReferenceData.getInstance().getLifterData().getShouldLift());
+	}
+	
+	@Test
+	public void testShouldLiftAlreadyFalseLiftUpTrueLiftDownTrue() {
+		ReferenceData.getInstance().getLifterData().setShouldLift(false);
+		ReferenceData.getInstance().getJoystickData().setLiftUp(true);
+		ReferenceData.getInstance().getJoystickData().setLiftDown(true);
+		lifterCalculator.teleopPeriodic();
+		assertFalse(ReferenceData.getInstance().getLifterData().getShouldLift());
+	}
+	
+	@Test
+	public void testShouldLiftAlreadyTrueLiftUpFalseLiftDownFalse() {
+		ReferenceData.getInstance().getLifterData().setShouldLift(true);
+		ReferenceData.getInstance().getJoystickData().setLiftUp(false);
+		ReferenceData.getInstance().getJoystickData().setLiftDown(false);
+		lifterCalculator.teleopPeriodic();
+		assertTrue(ReferenceData.getInstance().getLifterData().getShouldLift());
+	}
+	
+	@Test
+	public void testShouldLiftAlreadyTrueLiftUpFalseLiftDownTrue() {
+		ReferenceData.getInstance().getLifterData().setShouldLift(true);
+		ReferenceData.getInstance().getJoystickData().setLiftUp(false);
+		ReferenceData.getInstance().getJoystickData().setLiftDown(true);
+		lifterCalculator.teleopPeriodic();
+		assertFalse(ReferenceData.getInstance().getLifterData().getShouldLift());
+	}
+	
+	@Test
+	public void testShouldLiftAlreadyTrueLiftUpTrueLiftDownFalse() {
+		ReferenceData.getInstance().getLifterData().setShouldLift(true);
+		ReferenceData.getInstance().getJoystickData().setLiftUp(true);
+		ReferenceData.getInstance().getJoystickData().setLiftDown(false);
+		lifterCalculator.teleopPeriodic();
+		assertTrue(ReferenceData.getInstance().getLifterData().getShouldLift());
+	}
+	
+	@Test
+	public void testShouldLiftAlreadyTrueLiftUpTrueLiftDownTrue() {
+		ReferenceData.getInstance().getLifterData().setShouldLift(true);
+		ReferenceData.getInstance().getJoystickData().setLiftUp(true);
+		ReferenceData.getInstance().getJoystickData().setLiftDown(true);
+		lifterCalculator.teleopPeriodic();
+		assertFalse(ReferenceData.getInstance().getLifterData().getShouldLift());
+	}
+}
