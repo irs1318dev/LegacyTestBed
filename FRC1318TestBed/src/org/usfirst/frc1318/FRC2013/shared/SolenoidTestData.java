968b4c32684b490d20ba38eaa190a66486a917e8
Graham <Weason345@gmail.com>
2013-02-13 17:52:59 -0800

    added solenoidTestRunner and data

--- FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/shared/SolenoidTestData.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/shared/SolenoidTestData.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc1318.FRC2013.shared;
 
 public class SolenoidTestData {
+	public int state = 0;
+	public int loopCounter = 0;
+	public double startTime = 0;
 	public boolean fireIn, fireOut, fire;
 	public boolean shooterUp, shooterDown, shooterAngle;
 	public boolean armUp, armDown, armState;
