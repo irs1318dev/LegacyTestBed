f77dee0bf0841776a53eb9376c58af3fe26a1ca8
Graham <Weason345@gmail.com>
2013-02-14 20:59:46 -0800

    Joystick1 classes

--- FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/readers/Joystick1Reader.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/readers/Joystick1Reader.java
@@ -1,5 +1,31 @@
 package org.usfirst.frc1318.FRC2013.readers;
 
-public class Joystick1Reader {
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
+import org.usfirst.frc1318.FRC2013.reference.Joystick1Ref;
+import org.usfirst.frc1318.FRC2013.reference.PortRef;
+import org.usfirst.frc1318.FRC2013.shared.ReferenceData;
+import org.usfirst.frc1318.components.RobotComponentBase;
+
+public class Joystick1Reader extends RobotComponentBase {
+	Joystick joystick;
+	
+	public void robotInit() {
+		joystick = new Joystick(PortRef.JOYSTICK_R);
+	}
+
+	public void teleopPeriodic() {
+		ReferenceData.getInstance().getUserInputData().setLiftUp(joystick.getRawButton(Joystick1Ref.LIFT_UP));
+		ReferenceData.getInstance().getUserInputData().setLiftDown(joystick.getRawButton(Joystick1Ref.LIFT_DOWN));
+		ReferenceData.getInstance().getUserInputData().setShooterUp(joystick.getRawButton(Joystick1Ref.SHOOTER_UP));
+		ReferenceData.getInstance().getUserInputData().setShooterDown(joystick.getRawButton(Joystick1Ref.SHOOTER_DOWN));
+		ReferenceData.getInstance().getUserInputData().setShooterSpeedUp(joystick.getRawButton(Joystick1Ref.SHOOTER_SPEED_UP));
+		ReferenceData.getInstance().getUserInputData().setShooterSpeedDown(joystick.getRawButton(Joystick1Ref.SHOOTER_SPEED_DOWN));
+		ReferenceData.getInstance().getUserInputData().setShooterFire((joystick.getRawButton(Joystick1Ref.SHOOTER_FIRE)));
+		ReferenceData.getInstance().getUserInputData().setJoystickAxis1(joystick.getY(Hand.kLeft));
+		ReferenceData.getInstance().getUserInputData().setJoystickAxis2(joystick.getX(Hand.kLeft));
+	}
+	
+	
 }
--- FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/reference/Joystick1Ref.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/reference/Joystick1Ref.java
@@ -1,5 +1,21 @@
 package org.usfirst.frc1318.FRC2013.reference;
 
-public class Joystick1Ref {
-
+public interface Joystick1Ref {
+	static final int LIFT_UP = 11;
+	static final int LIFT_DOWN = 12;
+	
+	static final int SHOOTER_UP = 6;
+	static final int SHOOTER_DOWN = 7;
+	
+	static final int SHOOTER_FIRE = 1;
+	
+	static final int SHOOTER_SPEED_UP = 3;
+	static final int SHOOTER_SPEED_DOWN = 2;
+	
+	/*	These values were not used ^^^
+	static final int TEST_1 = 0;
+	static final int TEST_2 = 4;
+	static final int TEST_3 = 5;
+	static final int TEST_4 = 10;
+	*/
 }
