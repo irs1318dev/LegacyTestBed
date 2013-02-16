15740d78b8f59f15cc5761978705d1b337d125a6
Argeozonegon <Argeozonegon@Argon>
2013-02-15 17:20:32 -0800

    added stuff for autonomous macros.
    remember:
    -state diagram (with the circles)
    -state (the box)
    -squircle brackets
    -fancy little picture (with numbers)

--- FRC1318TestBed/src/org/usfirst/frc1318/autonomous/macros/AutoTranslateMacro.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/autonomous/macros/AutoTranslateMacro.java
@@ -1,38 +1,72 @@
 package org.usfirst.frc1318.autonomous.macros;
 
+import org.usfirst.frc1318.FRC2013.shared.ReferenceData;
 import org.usfirst.frc1318.autonomous.AutoTask;
 
 public class AutoTranslateMacro implements AutoTask
 {
-	private boolean isGoingRight;
+	private boolean hasInit = false;
+	private boolean hasFinished = false;
 	
-	public AutoTranslateMacro(boolean isGoingRight)
-	{
-		this.isGoingRight = isGoingRight;
-	}
+	public int state;
+	public int ticksForward1;
+	
 	
 	public void init() {
-		// TODO Auto-generated method stub
-		
+		// create things
+		state = 0;
+		hasInit = true;
 	}
 
-	public void run() {
-		// TODO Auto-generated method stub
+	public void run() 
+	{
+		switch(state)
+		{
+		case 0:
+			goForward1();
+			break;
+		case 1:
+			turnRight();
+			break;
+		case 2:
+			goForward2();
+			break;
+		case 3:
+			turnLeft();
+			break;
+		case 4:
+			goBackward();
+			break;
+		default:
+			hasFinished = true;
+			break;
+		}
 		
 	}
 
 	public void cancel() {
-		// TODO Auto-generated method stub
-		
+		// this is were you set any variables you were messing with
+		// to their default value, or the value before the Macro started.
 	}
 
 	public boolean hasFinished() {
-		// TODO Auto-generated method stub
-		return false;
+		return hasFinished;
 	}
 
 	public boolean hasInitalized() {
-		// TODO Auto-generated method stub
-		return false;
+		return hasInit;
 	}
+	
+	public void goForward1()
+	{// this is the first time we are moving forward.
+		if(ticksForward1 < 1080) // three wheel rotations.
+		{
+			ReferenceData.getInstance().getUserInputData().setJoystickLeft(1);
+		}
+		else
+		{
+			state = 1;
+		}
+	}
+	
 }
