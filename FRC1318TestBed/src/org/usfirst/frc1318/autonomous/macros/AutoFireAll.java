81cca4f54c19c781759f151f404a4e34d2299e76
IRS-003 <IRS-003@IRS-003-HP>
2013-02-16 18:44:05 -0800

    FireAll added

---
+++ FRC1318TestBed/src/org/usfirst/frc1318/autonomous/macros/AutoFireAll.java
@@ -0,0 +1,64 @@
+package org.usfirst.frc1318.autonomous.macros;
+
+//Assumes prior motor speed by driver preference.
+
+import org.usfirst.frc1318.FRC2013.reference.StateRef;
+import org.usfirst.frc1318.FRC2013.shared.ReferenceData;
+import org.usfirst.frc1318.autonomous.AutoTask;
+
+public class AutoFireAll implements AutoTask{
+
+	private boolean hasFinished = false;
+	private boolean hasInitialized = false;
+	private boolean newState = true;
+	
+	private int currentState;
+	private int  discsFired;
+	
+	public void init() {
+		discsFired = 0;
+		hasInitialized = true;
+	}
+
+	public void run() {
+		switch(currentState){
+			case 0://retract
+				break;
+			case 1://fire
+				break;//retract
+			default:
+				hasFinished = true;
+				break;
+		}
+		
+	}
+
+	public void retract(){
+		if(ReferenceData.getInstance().getShooterData().getShouldFire())
+			currentState++;		
+	}
+	
+	public void extend(){
+		if(discsFired<4){
+			ReferenceData.getInstance().getUserInputData().setShooterFire(true);
+			currentState = 0;
+			discsFired++;
+		}
+		else{
+			currentState++;
+		}
+	}
+	
+	public void cancel() {
+		ReferenceData.getInstance().getUserInputData().setShooterFire(false);
+	}
+
+	public boolean hasFinished() {
+		return hasFinished;
+	}
+
+	public boolean hasInitalized() {
+		return hasInitialized;
+	}
+
+}
