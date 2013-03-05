1fdf1e1a38dfd9fa499db68de0c02a97808d1c2c
Craig <craig.eden.k@hotmail.com>
2013-03-04 17:14:23 -0800

    Added LookUptable for shooter motor values

--- FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/shared/LookupTable.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/FRC2013/shared/LookupTable.java
@@ -3,5 +3,32 @@ package org.usfirst.frc1318.FRC2013.shared;
 import edu.wpi.first.wpilibj.networktables2.util.Set;
 
 public class LookupTable {
+	private final double[] SHOOTERVALUES = {0, -0.800, -0.885, -1.54};
+	public static int currentState;
 	
+	public double getValue()
+	{
+		//TODO: Rough understanding of throttle
+		return SHOOTERVALUES[currentState];// - ReferenceData.getInstance().getUserInputData().getThrottle();
+	}
+	
+	public void upSpeed()
+	{
+		//if(currentState == 3)
+		//	currentState = 0;
+		if(currentState <3)
+			currentState++;
+	}
+	
+	public void downSpeed()
+	{
+		//if (currentstate == 0)
+		//	currentState = 3;
+		if(currentState >0)
+			currentState--;
+	}
+
+	public void setCurrentState(int i) {
+		currentState = i;
+	}
 }
