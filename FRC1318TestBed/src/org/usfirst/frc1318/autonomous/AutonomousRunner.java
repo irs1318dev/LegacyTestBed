285e3edf869c226755cb5750fec4e759b78cbb20
issaquahroboticsociety@gmail.com <issaquahroboticsociety@gmail.com>
2013-03-21 10:23:41 -0700

    Autonomous middle (1&&2), side (1||2), nothing ()

--- FRC1318TestBed/src/org/usfirst/frc1318/autonomous/AutonomousRunner.java
+++ FRC1318TestBed/src/org/usfirst/frc1318/autonomous/AutonomousRunner.java
@@ -7,57 +7,44 @@ import org.usfirst.frc1318.components.RobotComponentBase;
 
 public class AutonomousRunner extends RobotComponentBase {
 	private AutoTask task;
-	
+
 	public void disabledInit() {
 		// when we disable, clear the AutoTask
 		task = null;
 	}
-	
+
 	public void autonomousPeriodic() {
 		if (task == null) {
 			System.out.println("No task - Checking dipswitch...");
-//			if (ReferenceData.getInstance().getDipSwitchData().getDipSwitch(DipSwitchRef.DIP_SWITCH_R) && ReferenceData.getInstance().getDipSwitchData().getDipSwitch(DipSwitchRef.DIP_SWITCH_L)) {
-//				System.out.println("Switch 2 on && switch 1 on - center");
-//				this.task = new AutonomousCenter();
-//			} else if (ReferenceData.getInstance().getDipSwitchData().getDipSwitch(DipSwitchRef.DIP_SWITCH_R)) {
-//				System.out.println("Switch 2 on && switch 1 off - right");
-//				this.task = new AutonomousRight();
-//			} else if (ReferenceData.getInstance().getDipSwitchData().getDipSwitch(DipSwitchRef.DIP_SWITCH_L)) {
-//				System.out.println("Switch 2 off && switch 1 on - left");
-//				this.task = new AutonomousLeft();
-//			} else {
-//				System.out.println("Switch 2 off && switch 1 off - nothing");
-//				this.task = null;
-//			}
-			
-			// For Ellensburg
-//			if(ReferenceData.getInstance().getDipSwitchData().getDipSwitch(DipSwitchRef.DIP_SWITCH_L)){
-//				System.out.println("Switch on - center");
-//				this.task = new AutonomousEllensburgMiddle();
-//			}else{
-//				System.out.println("Switch off - side");
-//				this.task = new AutonomousEllensburgSide();
-//			}
-			
-			//For practice bot
-			if(ReferenceData.getInstance().getDipSwitchData().getDipSwitch(DipSwitchRef.DIP_SWITCH_8)){
-				System.out.println("Switch on - center");
+			if (ReferenceData.getInstance().getDipSwitchData()
+					.getDipSwitch(DipSwitchRef.DIP_SWITCH_R)
+					&& ReferenceData.getInstance().getDipSwitchData()
+							.getDipSwitch(DipSwitchRef.DIP_SWITCH_L)) {
+				System.out.println("Switch 2 on && switch 1 on - center");
 				this.task = new AutonomousEllensburgMiddle();
-			}else{
-				System.out.println("Switch off - side");
+			} else if (ReferenceData.getInstance().getDipSwitchData()
+					.getDipSwitch(DipSwitchRef.DIP_SWITCH_R)) {
+				System.out.println("Switch 2 on && switch 1 off - right");
 				this.task = new AutonomousEllensburgSide();
+			} else if (ReferenceData.getInstance().getDipSwitchData()
+					.getDipSwitch(DipSwitchRef.DIP_SWITCH_L)) {
+				System.out.println("Switch 2 off && switch 1 on - left");
+				this.task = new AutonomousEllensburgSide();
+			} else {
+				System.out.println("Switch 2 off && switch 1 off - nothing");
+				this.task = null;
 			}
-		}
 
-		
-		if(task != null) {
-			if (!task.hasInitalized()) {
-				System.out.println("Autonomous init");
-				task.init();
-			} else if(!task.hasFinished()) {
-				System.out.println("Running Autonomous...");
-				task.run();
-			} 
+
+			if (task != null) {
+				if (!task.hasInitalized()) {
+					System.out.println("Autonomous init");
+					task.init();
+				} else if (!task.hasFinished()) {
+					System.out.println("Running Autonomous...");
+					task.run();
+				}
+			}
 		}
 	}
 }
